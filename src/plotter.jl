f5443abc75cd57610c646f3ffb6ba0b339ec5d56
Florian Oswald <florian.oswald@gmail.com>
2019-12-06 17:46:57 +0100

    update [skip ci]

--- src/plotter.jl
+++ src/plotter.jl
@@ -6,12 +6,20 @@
 
 function plot_static(m::Model,p::Param)
 	lvec = collect(range(0.,1.0,length=100))
+	gr()
+	xt = [0.0; 0.5;1.0]
 	plts = Any[]
-	push!(plts,plot(lvec,[τ(i,m.ϕ,p) for i in lvec], title = "tau",leg = false))
-	push!(plts,plot(lvec,[w(m.Lu,i,m.ϕ,p) for i in lvec], title = "w(l)",leg = false))
-	push!(plts,plot(lvec,[cu(i,p,m) for i in lvec], title = "cu(l)",leg = false))
-	push!(plts,plot(lvec,[cr(i,p,m) for i in lvec], title = "cr(l)",leg = false))
-	push!(plts,plot(lvec,[cost(i,m.ϕ,p) for i in lvec], title = "cost(l)",leg = false))
+	push!(plts,plot(lvec,[τ(i,m.ϕ,p) for i in lvec], title = "tau",leg = false, xticks = xt,titlefontsize=10,tickfont=font(6)))
+	push!(plts,plot(lvec,[w(m.Lu,i,m.ϕ,p) for i in lvec], title = "w(l)",leg = false, xticks = xt,titlefontsize=10,tickfont=font(6)))
+	push!(plts,plot(lvec,[cu(i,p,m) for i in lvec], title = "cu(l)",leg = false, xticks = xt,titlefontsize=10,xguidefontsize=6))
+	# push!(plts,plot(lvec,[cr(i,p,m) for i in lvec], title = "cr(l)",leg = false, xticks = xt,titlefontsize=10,guidefontsize=6))
+	push!(plts,plot(lvec,[cost(i,m.ϕ,p) for i in lvec], title = "cost(l)",leg = false, xticks = xt,titlefontsize=10,guidefontsize=6))
+	push!(plts,plot(lvec,[q(i,p,m) for i in lvec], title = "q(l)",leg = false, xticks = xt,titlefontsize=10,guidefontsize=6))
+	push!(plts,plot(lvec,[qr(i,p,m) for i in lvec], title = "qr(l)",leg = false, xticks = xt,titlefontsize=10,guidefontsize=6))
+	push!(plts,plot(lvec,[h(i,p,m) for i in lvec], title = "h(l)",leg = false, xticks = xt,titlefontsize=10,guidefontsize=6))
+	push!(plts,plot(lvec,[H(i,p,m) for i in lvec], title = "H(l)",leg = false, xticks = xt,titlefontsize=10,guidefontsize=6))
+	push!(plts,plot(lvec,[D(i,p,m) for i in lvec], title = "D(l)",leg = false, xticks = xt,titlefontsize=10,guidefontsize=6))
+	push!(plts,plot(lvec,[utility(i,p,m) for i in lvec], title = "U(l)",leg = false, xticks = xt,titlefontsize=10,guidefontsize=6))
 	plot(plts...)
 end
 
