81c6c063864aeb9c7f084fbc04c19ea9eb153e51
Florian Oswald <florian.oswald@gmail.com>
2020-02-13 18:27:19 +0100

    identical starting values with 4 eqsys (and nlopt) vs 6 eqsys (and nlsolve) but more robust to changing L and S. refers to issue #5 .

--- src/startvals.jl
+++ src/startvals.jl
@@ -1,4 +1,50 @@
 
+
+# new strategy to find starting values:
+# use a constrained optimizer to avoid x < 0
+# this works for the fixed model so far.
+# julia> x = LandUse.nlopt_solve(par = Dict(:L => 100.0, :S => 20.0))
+# (1.0, [0.04796005781257872, 0.4480817574692655, 0.3219286166291397, 14.974867748022646], :ROUNDOFF_LIMITED)
+
+
+function NLopt_wrap(result::Vector, x::Vector, grad::Matrix,m::Model,p::Param)
+	if length(grad) > 0
+		# not implemented
+	end
+	solve2!(result,x,p,m)
+end
+
+function nlopt_solve(;p = Param(),x0=nothing)
+	fm = FModel(p)
+	opt = Opt(:LN_COBYLA,4)
+	# opt = Opt(:LN_NELDERMEAD,4)
+	opt.lower_bounds = fill(0.001,4)
+	# opt.upper_bounds = [1.0,1.0,1.0,1.0]
+	f0(x::Vector, grad::Vector) = 1.0
+	opt.min_objective = f0  # fix at a constant
+	equality_constraint!(opt,(r,x,g) -> NLopt_wrap(r,x,g,fm,p), fill(1e-9,4))
+	# m.r    = x[1]   # land rent
+	# m.Lr   = x[2]   # employment in rural sector
+	# m.pr   = x[3]   # relative price rural good
+	# m.Sr   = x[4]   # amount of land used in rural production
+	# (optf,optx,ret) = optimize(opt, [0.1 * p.S, 0.5 * p.L, 0.3775, 0.545 * p.S])
+	if isnothing(x0)
+		x0 = [0.1 * p.S, 0.5 * p.L, 0.3775, 0.545 * p.S]
+	else
+		@assert length(x0) == ndims(opt)
+	end
+	(optf,optx,ret) = optimize(opt, x0)
+end
+
+
+
+
+
+
+
+
+
+
 # models to generate starting value for full model
 
 """
@@ -158,6 +204,45 @@ function update!(m::Region,m0::CD0Model,p::Param)
 end
 
 
+function get_starts2(;par = Dict())
+	p = Param(par=par)
+	fm = LandUse.FModel(p)  # create a fixed elasticity model
+	startvals = Vector{Float64}[]  # an empty array of vectors
+
+	# 2. For each time period
+	for it in 1:length(p.T)
+		setperiod!(p, it)   # set period on param to it
+
+		# if first year
+		if it == 1
+			x0 = nlopt_solve(p=p)
+			if (x0[3] == :ROUNDOFF_LIMITED) | (x0[3] == :LIMITED)
+				update2!(fm,p,x0[2])
+				push!(startvals, x0[2])
+				# println("rural market clears with $(Rmk(fm,p))")
+			else
+				# p1 = plot(fm.Ftrace',ylim = (-5,5),title = "Ftrace")
+				# p2 = plot(fm.xtrace',title = "xtrace",label = ["rho" "phi" "r" "Lr" "pr" "Sr"])
+				# pl = plot(p1,p2,layout = (1,2))
+				# savefig(pl,joinpath(@__DIR__,"..","images","Fmodel_trace.png"))
+				error("first FModel not converged")
+			end
+
+		else  # in other years just start at previous solution
+			x0 = nlopt_solve(p=p,x0 = startvals[it-1])
+			if (x0[3] == :ROUNDOFF_LIMITED) | (x0[3] == :LIMITED)
+				update2!(fm,p,x0[2])
+				push!(startvals, x0[2])
+				# println("rural market clears with $(Rmk(fm,p))")
+			else
+				print(x0)
+				error("FModel not converged in period $it")
+			end
+		end
+	end
+	return startvals
+end
+
 
 """
 	get_starts(;par = Dict())
@@ -206,7 +291,7 @@ function get_starts(;par = Dict())
 			end
 
 
-				x00 = [m0.ρr; 0.00005; m0.r; m0.Lr; m0.pr; m0.Sr - 0.00005]   # set very small city!
+				# x00 = [m0.ρr; 0.00005; m0.r; m0.Lr; m0.pr; m0.Sr - 0.00005]   # set very small city!
 
 
 			# 4. solve general model with fixed elasticity starting from x00
@@ -222,10 +307,10 @@ function get_starts(;par = Dict())
 				push!(startvals, r1.zero)
 				# println("rural market clears with $(Rmk(fm,p))")
 			else
-				p1 = plot(fm.Ftrace',ylim = (-5,5),title = "Ftrace")
-				p2 = plot(fm.xtrace',title = "xtrace",label = ["rho" "phi" "r" "Lr" "pr" "Sr"])
-				pl = plot(p1,p2,layout = (1,2))
-				savefig(pl,joinpath(@__DIR__,"..","images","Fmodel_trace.png"))
+				# p1 = plot(fm.Ftrace',ylim = (-5,5),title = "Ftrace")
+				# p2 = plot(fm.xtrace',title = "xtrace",label = ["rho" "phi" "r" "Lr" "pr" "Sr"])
+				# pl = plot(p1,p2,layout = (1,2))
+				# savefig(pl,joinpath(@__DIR__,"..","images","Fmodel_trace.png"))
 				error("first FModel not converged")
 			end
 
